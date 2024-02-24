288ba3168e4781f248ac9f20e373a119453a8546
Widney Lima <widneylima21@gmail.com>
2024-02-23 23:46:49 -0300

    2º commit - adicionando a annotation @CrossOrigin na classe de controle para permitir o trafego de requisições http

--- src/main/java/com/widneydev/pantheon_api/controle/UsuarioControle.java
+++ src/main/java/com/widneydev/pantheon_api/controle/UsuarioControle.java
@@ -2,6 +2,7 @@ package com.widneydev.pantheon_api.controle;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import com.widneydev.pantheon_api.servico.UsuarioServico;
 
 
 @RestController
+@CrossOrigin(origins = "*")
 public class UsuarioControle {
 
     @Autowired 
