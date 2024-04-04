7951bd808486748c062249b1be7a89fd61c506be
Widney Lima <widneylima21@gmail.com>
2024-04-03 23:18:37 -0300

    Atualização de pastas

---
+++ src/test/java/com/widneydev/pantheon_api/PantheonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.widneydev.pantheon_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PantheonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
